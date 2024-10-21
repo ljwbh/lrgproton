4afbadb49ce2f77d3cb51f1843a968d7cfe02e79
ljwbh <41594793+ljwbh@users.noreply.github.com>
2024-10-21 09:09:46 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,12 +6,14 @@ let mytoken= ['auto'];//快速订阅访问入口, 留空则不启动快速订阅
 // 设置优选地址，不带端口号默认443，TLS订阅生成
 let addresses = [
 	'ca.ljwbh.filegear-sg.me:443#优选－B',
-	'poppy.ns.cloudflare.com:443#poppy',
 	'thaddeus.ns.cloudflare.com:443#thaddeus',
 	'thaddeus.ns.cloudflare.com:443#thaddeus',
-	'romina.ns.cloudflare.com:443#romina',
 	'www.iios.me:443#www.iios.me',
 	'www.iios.club:443#www.iios.club',
+	'www.visa.com.sg:443#visa.com.sg',
+	'www.visa.com.tw:443#visa.com.tw',
+	'www.visa.co.jp:443#visa.co.jp',
+	'www.digitalocean.com:443#digitalocean',
 ];
 // 设置优选地址api接口
 let addressesapi = [
